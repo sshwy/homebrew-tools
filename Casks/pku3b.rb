7b6cdd9f74abe49e93bae92b5b157317b063b99b
sshwy <jy.cat@qq.com>
2026-04-22 00:07:06 +0800

    pku3b 0.12.1

--- Casks/pku3b.rb
+++ Casks/pku3b.rb
@@ -2,8 +2,8 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "pku3b" do
-  version "0.12.0"
-  sha256 "9aee806e140bbe82dcb8113e051e5053cfa65cb39f4b12ebbef8d9b488775a84"
+  version "0.12.1"
+  sha256 "a369583d69d4196d1a47d895fbd082614e7825855ca1c331752f4a24d160102f"
 
   url "https://github.com/sshwy/pku3b/releases/download/#{version}/pku3b-#{version}-aarch64-apple-darwin.tar.gz"
   name "pku3b"
